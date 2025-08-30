76e510eea85d75426869c03b58fc8d22b4bb2b2b
Todd Polak <toddpolakdev@gmail.com>
2025-08-30 18:48:18 -0500

    ts fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { lightTheme, darkTheme } from "./theme";
